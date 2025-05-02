47ee23510e59fefbfb71aedb27de2d4d7de7b783
xasale <alexandrosastros@gmail.com>
2025-05-02 16:10:49 +0300

    2nd commit

--- src/main/java/com/xasale/excel2JSON/ExcelProcessingHelper.java
+++ src/main/java/com/xasale/excel2JSON/ExcelProcessingHelper.java
@@ -19,7 +19,7 @@ public class ExcelProcessingHelper {
             // int sheetIndex = workbook.getActiveSheetIndex();
 
             //get sheet
-            XSSFSheet sheet = workbook.getSheetAt(1);
+            XSSFSheet sheet = workbook.getSheetAt(0);
 
             System.out.println("Το current active sheet number είναι: " + sheet);
 
